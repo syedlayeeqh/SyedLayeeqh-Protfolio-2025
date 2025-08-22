43c29baa153db545948335adbde21bc1b7906e0b
Aftab Ahamed <aftabahamedvm@gmail.com>
2025-08-22 14:15:31 +0530

    Add new feature

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,14 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => ({
-  // 👇 THIS is the important line
+// https://vitejs.dev/config/
+export default defineConfig({
   base: "/SyedLayeeqh-Protfolio-2025/",
-
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
