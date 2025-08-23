81e6dff5a1b47aef49f6ddc19a2780e6a2a011d0
Aftab Ahamed <aftabahamedvm@gmail.com>
2025-08-23 19:23:45 +0530

    base path fix - App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/SyedLayeeqh-Protfolio-2025">
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/case-study/MindMate" element={<CaseStudyMindMate />} />
